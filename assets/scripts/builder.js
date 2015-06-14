6d633181d0e531e0d87bd3220086509cbbfd145a
Julien <ngc_master@hotmail.fr>
2015-06-15 01:52:34 +0200

    Chargement des données

--- assets/scripts/builder.js
+++ assets/scripts/builder.js
@@ -1,8 +1,23 @@
-(function ($) {
+$(document).ready(function () {
 	'use strict';
 	//var professions = ['elementalist', 'engineer', 'guardian', 'mesmer', 'necromancer', 'ranger', 'revenant', 'thief', 'warrior'];
 	var $builder = $('#builder');
-	var CURRENT_PROFESSION = '';
+
+	var BASE_STATS = {
+		'health': 924,
+		'armor': 924,
+		'power': 924,
+		'precision': 924,
+		'ferocity': 0,
+		'healing': 0,
+		'condition': 0
+	},
+		CURRENT_PROFESSION = '',
+		CURRENT_SPECIALIZATIONS = {},
+		CURRENT_WEAPONS = {},
+		CURRENT_SKILLS = {},
+		CURRENT_STATS = {};
+
 	$builder.find('#professionSelection div').each(function () {
 		$(this).on('click', function () {
 			$('.selected').removeClass('selected');
@@ -14,5 +29,22 @@
 
 	function loadProfession(profession) {
 		CURRENT_PROFESSION = profession;
+		var url = './data/' + profession + '.json';
+
+		$.get(url,
+			function (data) {
+				parseProfessionData(data);
+			},
+			'json'
+		);
 	}
-})(jQuery);
+
+	function parseProfessionData(data) {
+		CURRENT_SPECIALIZATIONS = data.specializations;
+		CURRENT_WEAPONS = data.weapons;
+		CURRENT_SKILLS = data.skills;
+		CURRENT_STATS = data.stats;
+
+		BASE_STATS += CURRENT_STATS;
+	}
+});
